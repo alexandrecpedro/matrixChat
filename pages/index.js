83fa2fce31cc21b1b854a14a78c1eed1f7ca86ab
alexandrecpedro <alexandrecpedro@gmail.com>
2022-01-24 16:13:56 -0300

    Matrix Chat - Aluracord [1st step]

--- pages/index.js
+++ pages/index.js
@@ -94,7 +94,7 @@ export default function HomePage() {
                         width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
                         }}
                     >
-                        <Title tag="h2">Boas vindas de volta!</Title>
+                        <Title tag="h2">Boas-vindas de volta!</Title>
                         <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                         {appConfig.name}
                         </Text>
